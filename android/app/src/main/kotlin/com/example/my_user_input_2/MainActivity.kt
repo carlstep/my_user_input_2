a5b038bd6fdad471123f102ac220da18d0b9c2da
carlstep <carlstep333@me.com>
2022-08-31 10:42:35 +0700

    initial code for billAmount

---
+++ android/app/src/main/kotlin/com/example/my_user_input_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_user_input_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'User Input',
+      home: BillProcess(),
+    );
+  }
+}
+
+class BillProcess extends StatefulWidget {
+  const BillProcess({Key? key}) : super(key: key);
+
+  @override
+  State<BillProcess> createState() => _BillProcessState();
+}
+
+class _BillProcessState extends State<BillProcess> {
+  final myController = TextEditingController();
+  double billAmount = 0;
+  String billCurrency = 'USD';
+  double billAmountConverted = 0;
+  String billCurrencyConverted = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bill Process'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                onChanged: (value) {
+                  setState(() {
+                    billAmount = double.parse(value);
+                    if (billAmount >= 1000) {
+                      billCurrency = 'KHR';
+                      billAmountConverted = billAmount / 4000;
+                      billCurrencyConverted = 'USD';
+                    } else {
+                      billCurrency = 'USD';
+                      billAmountConverted = billAmount * 4000;
+                      billCurrencyConverted = 'KHR';
+                    }
+                  });
+                },
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+                controller: myController,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text('$billAmountConverted $billCurrencyConverted'),
+              const SizedBox(
+                height: 20,
+              ),
+              Text('This is the bill currency - $billCurrency'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
